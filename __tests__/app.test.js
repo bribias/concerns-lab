9a1aaed61d177869b8aafb3650422430e327d9cd
Brianna Bias <brianna.bias@gmail.com>
2021-07-22 11:03:08 -0700

    get all orders

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -39,15 +39,15 @@ describe('03_separation-of-concerns-demo routes', () => {
       });
   });
 
-  // it('gets an all orders', async () => {
-  //   const order = await Order.insert({ quantity: 10 });
+  it('gets an all orders', async () => {
+    const order = await Order.insert({ quantity: 10 });
 
-  //   return request(app)
-  //     .get(`/api/v1/orders/${order.id}`)
-  //     .then((res) => {
-  //       expect(res.body).toEqual(order);
-  //     });
-  // });
+    return request(app)
+      .get(`/api/v1/orders/${order.id}`)
+      .then((res) => {
+        expect(res.body).toEqual(order);
+      });
+  });
 
   // it('updates order', async () => {
   //   const order = await Order.insert({ quantity: 10 });
